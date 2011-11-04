ff1c8f62b5407473ce0c8a464f0aa65925c85de2
Leo <ateista@gmail.com>
2011-11-04 16:46:25 -0200

    'first'

---
+++ CheckedItemsConverter.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace ComboCheckBox
+{
+    using System;
+    using System.Windows.Data;
+    using System.Collections;
+    using System.Linq;
+    using System.Collections.Generic;
+
+
+    public class CheckedItemsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var col = value as IEnumerable<CheckItem>;
+            if (col != null)
+            {
+                var txt = String.Join("," , col.Where(c=>c.IsChecked == true).Select(c=>c.Text));
+                return txt;
+            }
+            return null;
+        }
+
+     
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();            
+        }        
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace ComboCheckBox
+{
+    public partial class MainPage : UserControl, INotifyPropertyChanged
+    {
+
+        public ObservableCollection<CheckItem> List { get; set; }
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+            List = new ObservableCollection<CheckItem>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                var item = new CheckItem() { Text = "Item" + i.ToString(), IsChecked = false };
+                item.PropertyChanged += (sd, args) => { NotifyPropertyChanged("List"); };
+                List.Add(item);
+            }
+
+
+            
+            
+            
+
+            this.DataContext = this;
+
+
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(String info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var checkedItems = List.Where(i => i.IsChecked == true);
+
+            checkedItems.ToList().ForEach(i => MessageBox.Show(i.Text));
+        }
+
+    }
+
+
+
+    public class CheckItem : INotifyPropertyChanged
+    {
+        public string Text { get; set; }
+
+        private bool isChecked;
+        public bool IsChecked
+        {
+            get { return isChecked; }
+            set
+            {
+                isChecked = value;
+                NotifyPropertyChanged("IsChecked");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(String info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+    }
+}
+
+
+
